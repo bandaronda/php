70575291ae1f438db95ddd24bf1ff4c44ec65328
bandaronda <131749924+bandaronda@users.noreply.github.com>
2023-05-11 14:41:25 +0200

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<?php
+
+//dit is opdracht 1
+$a= 10;
+$b= 12;
+
+$a=$a+$b;
+
+//opdracht 2
+$c= 10;
+$t= 12;
+
+$c=$c*$t;
+
+// opdracht 3
+
+$naam="Lenny";
+
+echo "welkom".$naam;
+
+//opdracht 4
+
+// deze code bevat veel fouten
+//haal alle fouten eruit!
+
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo " zit bij ".$mijnNaam.$jouwNaam." in de klas!";
+
+
+?>
